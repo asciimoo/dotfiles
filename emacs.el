04ff75d9e97929745f3095a5463839968dcd43bf
Adam Tauber <asciimoo@gmail.com>
2016-12-29 00:54:20 +0100

    [enh] emacs config update II.

--- emacs.el
+++ emacs.el
@@ -1,42 +1,50 @@
 ;;; .emacs --- CONFIG
 (require 'cl)
 (require 'package)
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 (defvar required-packages
   '(
     evil
-    go-mode
-
-    helm
-    helm-projectile
-
-    markdown-mode
     molokai-theme
     relative-line-numbers
     smooth-scrolling
     spaceline-config
-    yaml-mode
     yasnippet
+    ;; evil
+    evil
+    evil-leader
+    evil-org
+    ;; helm
+    helm
+    helm-projectile
+    ;; org
+    org
+    ;; language specific packages
+    go-mode
+    markdown-mode
+    yaml-mode
    )
 )
 
-;define package installer
+;; define package installer
 (defun install-packages ()
   (interactive)
   (dolist (p required-packages)
     (when (not (package-installed-p p))
       (package-install p))))
 
-;load required packages
+;; load required packages
 (loop for p in required-packages
   do (require p))
 
-; UI
+;; UI
 (load-theme 'molokai t)
 (set-background-color "black")
+(set-face-foreground 'font-lock-comment-face "dark grey")
+(set-face-foreground 'font-lock-comment-delimiter-face "dark grey")
 (setq-default show-trailing-whitespace t)
 (set-face-attribute 'default nil :height 80)
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -54,45 +62,46 @@
 (smooth-scrolling-mode 1)
 (setq scroll-preserve-screen-position t)
 
-; CUSTOMISATIONS
-;use 4 spaces instead of tabs
+;; CUSTOMISATIONS
+;; use 4 spaces instead of tabs
 (setq-default tab-width 4 indent-tabs-mode nil)
-;don't create backup files
+;; don't create backup files
 (setq make-backup-files nil)
 (setq backup-inhibited t)
-;copy/paste
+;; copy/paste
 (setq select-active-regions nil)
 (setq mouse-drag-copy-region t)
 (setq x-select-enable-primary t)
 (global-set-key [mouse-2] 'mouse-yank-at-click)
-;history
+;; history
 (savehist-mode 1)
 (add-to-list 'savehist-additional-variables 'regexp-search-ring)
-;disable autosave
+;; disable autosave
 (setq auto-save-default nil)
 
-; KEY BINDINGS
+;; KEY BINDINGS
 (global-set-key [f8] 'global-relative-line-numbers-mode)
 (global-set-key [f9] 'toggle-menu-bar-mode-from-frame)
 
-; PACKAGE SPECIFIC SETTINGS
-; EVIL MODE
+;; PACKAGE SPECIFIC SETTINGS
+
+;; EVIL MODE
 (evil-mode 1)
 (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
 (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
 (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
 (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
 
-; FLYCHECK
-;(global-flycheck-mode)
+;; FLYCHECK
+;; (global-flycheck-mode)
 
-; SPACELINE
+;; SPACELINE
 (spaceline-spacemacs-theme)
 
-; YASNIPPET
+;; YASNIPPET
 (yas-global-mode 1)
 
-; HELM
+;; HELM
 (require 'helm-config)
 (setq helm-split-window-in-side-p           nil
       helm-move-to-line-cycle-in-source     t
@@ -115,10 +124,17 @@
 (helm-projectile-on)
 (helm-mode 1)
 
-; GO-MODE
+;; ORG
+(global-set-key "\C-ca" 'org-agenda)
+(setq org-directory "~/d/org")
+(setq org-agenda-files '("~/d/org/todo.org"
+                         "~/d/org/searx.org"))
+(setq org-todo-keywords '((sequence "TODO(t)" "WAIT(w@/!)" "|" "DONE(d!)" "CANCELED(c@)")))
+
+;; GO-MODE
 (add-hook 'before-save-hook 'gofmt-before-save)
 
-; YAML-MODE
+;; YAML-MODE
 (add-hook 'yaml-mode-hook
   '(lambda ()
       (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
